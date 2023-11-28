dfacafb2d0740a8c639ecc6bf5e411180de415d4
Hasan <thehasandev@gmail.com>
2023-11-28 20:04:30 +0600

    add-goggle-font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'pop': ['Poppins'],
+        'roboto': ['Roboto'],
+      }
+    },
   },
   plugins: [],
 }
