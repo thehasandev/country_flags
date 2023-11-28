b07264181b7da1127326056f89a666c444748d02
Hasan <thehasandev@gmail.com>
2023-11-28 20:21:16 +0600

    add-btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 
+import "./App.css"
 import one from "./assets/a.jpg"
+
 
 function App() {
   return (
     <div className='max-w-7xl mx-auto'>
         <h1 className='font-bold font-roboto text-5xl text-[#111111] text-center'>Country App</h1>
         <div className='flex'>
-           <div className='w-96 bg-red-500'>
+           <div className='w-96 bg-white shadow-xl rounded-[4px]'>
             <div>
-              <img src={one} alt="" className='h-46 w-full' />
+              <img src={one} alt="" className='h-46 w-full rounded-t-[4px]' />
             </div>
-            <div className='px-5'>
-               <h1 className='font-roboto font-bold text-3xl text-[#111111] text-center'>Bangledesh</h1>
-               <p className='font-roboto font-normal text-base text-[#111111]'>Capital: 14444444</p>
-               <p className='font-roboto font-normal text-base text-[#111111]'>Area: 14444444</p>
-               <p className='font-roboto font-normal text-base text-[#111111]'>discription</p>
+            <div className='p-5'>
+               <h1 className='font-roboto font-bold text-3xl text-[#111111] text-center mb-4'>Bangledesh</h1>
+               <p className='font-roboto font-normal text-base mb-2 text-[#111111]'>Capital: 14444444</p>
+               <p className='font-roboto font-normal text-base mb-2 text-[#111111]'>Area: 14444444</p>
+               <p className='font-roboto font-normal text-base mb-4 text-[#111111]'>discription</p>
+               <div className='flex justify-center'>
+                <button className='font-roboto font-medium text-lg text-white bg-red-500 px-6 py-2 rounded-[5px]'>Delete Country</button>
+               </div>
             </div>
 
            </div> 
